a8cc6544032cb2d120494af82fa9f0e7dfee70e7
kavorite <iseurie@gmail.com>
2019-06-26 12:37:30 -0400

    return to set exclusion

--- exclusion.py
+++ exclusion.py
@@ -1,18 +1,16 @@
 import csv
 import re
 from usps_abbv import ABBREVIATIONS as ABBV
-from collections import Counter
 
 
 class StreetAddress(object):
-    def __init__(self, city: str, postcode: int, st: str, nr: int):
+    def __init__(self, city: str, st: str, nr: str):
         self.city = city.lower().strip()
         self.street = self.__class__.normalize(st)
         self.number = nr
-        self.zip = postcode
 
     def tuple(self):
-        return (self.city, self.zip, self.street, self.number)
+        return (self.city, self.street, self.number)
 
     @staticmethod
     def normalize(street):
@@ -45,28 +43,14 @@ class MonroeCtRecord(object):
 
     def address(self) -> StreetAddress:
         st = f'{self.loc_st_name} {self.loc_st_type}'
-        return StreetAddress(self.city, int(self.par_zip), st, int(self.st_nbr))
+        return StreetAddress(self.city, st, self.st_nbr)
 
     def tuple(self):
         return tuple(getattr(self, k) for k in self.__class__.__slots__)
-            
-    @staticmethod
-    def parse_family_count(res_kind: str):
-        res_kind = res_kind.lower().strip()
-        count_pattern = re.compile('([0-9]+) family res')
-        counts = count_pattern.finditer(res_kind)
-        try:
-            count = int(next(counts).groups()[0])
-        except (StopIteration, IndexError):
-            count = -1
-        return count
-    
-    def families(self):
-        n = self.__class__.parse_family_count(self.prop_desc)
-        # assume an "infinite" count of families for bulk housing and edge
-        # contingencies
-        return n if n > 0 else float('inf')
 
+    def is_bulk(self):
+        return 'family res' not in self.prop_desc.lower()
+            
 
 def universe(istrm):
     '''
@@ -86,38 +70,18 @@ def registered(istrm):
     current registrants by counting their occurrences in the given record
     stream.
     '''
-    rtn = Counter()
     BOEIDX_CITY = 11
     BOEIDX_DLVY_NR = 5
     BOEIDX_DLVY_ST = 6
-    BOEIDX_ZIP = 13
+    # BOEIDX_ZIP = 13
     rows = csv.reader(istrm)
     next(rows)  # discard header
     for ent in rows:
-        try:
-            record = StreetAddress(
-                    ent[BOEIDX_CITY],
-                    int(ent[BOEIDX_ZIP]),
-                    ent[BOEIDX_DLVY_ST],
-                    int(ent[BOEIDX_DLVY_NR]))
-            rtn[record] += 1
-        except ValueError:
-            continue
-    return rtn
-
-
-def exclusion(U, R):
-    '''
-    Returns a generator that excludes Monroe County address records with at
-    least as many registered voters as they have resident families from its
-    output. Filter always succeeds for bulk housing and other contingencies.
-    '''
-    for ent in U:
-        try:
-            if R[ent.address()] < ent.families():
-                yield ent
-        except ValueError:
-            continue
+        record = StreetAddress(
+                ent[BOEIDX_CITY],
+                ent[BOEIDX_DLVY_ST],
+                ent[BOEIDX_DLVY_NR])
+        yield record
 
 
 if __name__ == '__main__':
@@ -138,11 +102,10 @@ if __name__ == '__main__':
     U = universe(istrm)
     header = next(U)
 
-    # Create a mapping of addresses to their current nr. of registered voters
     istrm = open(args.registered)
-    R = registered(istrm)
+    R = set(registered(istrm))
 
-    X = exclusion(U, R)
+    X = (ent for ent in U if ent.is_bulk() or ent.address() not in R)
 
     if args.opath is None:
         from sys import stdout
